ae2f19ddb32b55d4bf3b36144edd31d36fc4b0f8
zhaozongyuan <804898662@qq.com>
2018-06-24 13:14:02 +0800

    mybatis 逆向生成

---
+++ mybatis_generator/src/com/sxt/mapper/RoleMapper.java
@@ -0,0 +1,17 @@
+package com.sxt.mapper;
+
+import com.sxt.pojo.Role;
+
+public interface RoleMapper {
+    int deleteByPrimaryKey(Integer roleid);
+
+    int insert(Role record);
+
+    int insertSelective(Role record);
+
+    Role selectByPrimaryKey(Integer roleid);
+
+    int updateByPrimaryKeySelective(Role record);
+
+    int updateByPrimaryKey(Role record);
+}
